f9388ff67fcf5d6977c784ed7d8a6d2bc672488b
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-09 19:43:37 +0200

    working on install file

--- install.sh
+++ install.sh
@@ -2,3 +2,15 @@
 
 git checkout publish
 git pull
+
+cd code
+chmod 755 start.sh
+chmod 755 stop.sh
+
+crontab -l < cron.cfg
+
+echo '0 21 * * * /root/Folding-Service/start.sh' >> cron.cfg
+echo '0 6 * * * /root/Folding-Service/stop.sh' >> cron.cfg
+crontab cron.cfg
+
+rm cron.cfg
