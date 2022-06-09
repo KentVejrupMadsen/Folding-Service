bd5ab1ac523dd196bae3bc06b3e50049ecfe98f8
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-09 20:03:06 +0200

    c

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-if (( !$EUID == 0 )); then
-    echo 'requires root access to install';
-    exit
-fi
+echo 'requires root access to install'
 
+if (( $EUID == 0 )); then
+    echo "is root"
 
-cd /root
-git clone https://github.com/KentVejrupMadsen/Folding-Service.git
+    cd /root
+    git clone https://github.com/KentVejrupMadsen/Folding-Service.git
 
-cd Folding-Service
+    cd Folding-Service
 
-git checkout publish
-git pull
+    git checkout publish
+    git pull
 
-cd code
-chmod 755 start.sh
-chmod 755 stop.sh
+    cd code
+    chmod 755 start.sh
+    chmod 755 stop.sh
 
-crontab -l < cron.cfg
+    crontab -l < cron.cfg
 
-echo '0 21 * * * /root/Folding-Service/start.sh' >> cron.cfg
-echo '0 6 * * * /root/Folding-Service/stop.sh' >> cron.cfg
-crontab cron.cfg
+    echo '0 21 * * * /root/Folding-Service/start.sh' >> cron.cfg
+    echo '0 6 * * * /root/Folding-Service/stop.sh' >> cron.cfg
+    crontab cron.cfg
 
-rm cron.cfg
+    rm cron.cfg
+fi;
