2bc44981b4b9373be9b855f388a8d3d2211412af
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-09 20:11:04 +0200

    update

--- install.sh
+++ install.sh
@@ -17,10 +17,10 @@ if (( $EUID == 0 )); then
     chmod 755 start.sh
     chmod 755 stop.sh
 
-    crontab -l < cron.cfg
+    crontab -l >> cron.cfg
 
-    echo '0 21 * * * /root/Folding-Service/start.sh' >> cron.cfg
-    echo '0 6 * * * /root/Folding-Service/stop.sh' >> cron.cfg
+    echo '0 21 * * * /root/Folding-Service/code/start.sh' >> cron.cfg
+    echo '0 6 * * * /root/Folding-Service/code/stop.sh' >> cron.cfg
     crontab cron.cfg
 
     rm cron.cfg
